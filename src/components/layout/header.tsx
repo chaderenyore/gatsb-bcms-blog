9a4537ee990c3ca5a362886e3113592d900737c6
Momcilo Popov <popovmomcilo@gmail.com>
2020-10-26 13:47:31 +0100

    Update components

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 const Header: React.FunctionComponent = () => {
   return <div>Header</div>;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import Footer from './footer';
 import HeadMeta from './head-meta';
 import Header from './header';
