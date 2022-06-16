c109212be4f108625202fd50e51438e92fd01fdd
Bbanez <banezprod@gmail.com>
2022-06-16 14:51:16 +0200

    Fix var name

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,7 +26,7 @@ const config: GatsbyConfig = {
           origin: process.env.BCMS_API_ORIGIN || 'https://becomes-starter-projects.yourbcms.com',
           key: {
             id: process.env.BCMS_API_KEY || '629dcd4dbcf5017354af6fe8',
-            secret: process.env.BCMS_API_KEY_SECRET || '7a3c5899f211c2d988770f7561330ed8b0a4b2b5481acc2855bb720729367896'
+            secret: process.env.BCMS_API_SECRET || '7a3c5899f211c2d988770f7561330ed8b0a4b2b5481acc2855bb720729367896'
           }
         },
         media: {
